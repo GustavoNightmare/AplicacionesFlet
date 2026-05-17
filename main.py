11d18c93b1b4732c428e5aef6f5723a643d44c54
GustavoSandoval <gsandovalm@unicauca.edu.co>
2026-05-16 23:13:34 -0500

    Inicio

---
+++ main.py
@@ -0,0 +1,15 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title="Background Remover Pro"
+    page.bgcolor ="#1a1a2e"
+    page.window.height=1080
+    page.window.width =1920
+    page.theme_mode =ft.ThemeMode.DARK
+    page.update
+    page.add(
+        ft.Text("Hola adrentro", color="white")
+    )
+  
+
+ft.app(target=main)
